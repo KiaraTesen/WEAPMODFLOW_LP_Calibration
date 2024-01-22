c3ab50e71d553c801e7c48c579fee2ad948a6156
KiaraTesen <katesen@uc.cl>
2024-01-22 13:13:07 -0500

    Sube muestreo 15 maq

--- ADPSO-CL/PreMethodology_ADPSO_CL.py
+++ ADPSO-CL/PreMethodology_ADPSO_CL.py
@@ -10,7 +10,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 #---    Initial matriz
-n = 3                                                # Population size: 20 o 35
+n = 15                                                # Population size: 10 o 15
 
 active_cells = 18948
 
