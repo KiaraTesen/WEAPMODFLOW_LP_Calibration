def06efc78c6d48a0cb9c62a9ad2acca1ca0895c
KiaraTesen <katesen@uc.cl>
2024-03-01 17:23:59 -0500

    Prueba recuperacion corridas

--- ADPSO-CL/Methodology_ADPSO_CL.py
+++ ADPSO-CL/Methodology_ADPSO_CL.py
@@ -70,92 +70,193 @@ class Particle:
 pob = Particle(np.around(np.array([0]*(n_var)),4),np.around(np.array([0]*(n_var)),4),10000000000)
 
 #---    Initial Sampling - Pob(0)
-ITERATION = 0
+dir_recap = os.path.join(path_output + '/ADPSO_CL_register_vm' + str(VM) + '.h5')
+if not os.path.isfile(dir_recap):
+    ITERATION = 0
+else:
+    with h5py.File('ADPSO_CL_register_vm' + str(VM) + '.h5', 'r') as f:
+        n_recap = len(f["pob_x"][:])
+    ITERATION = n_recap
+print(ITERATION)
 
-with h5py.File('Pre_ADPSO-CL.h5', 'r') as f:
-    pob.x = np.copy(f["pob_x"][VM-2])
-f.close()
+if ITERATION == 0:
 
-y_init = Run_WEAP_MODFLOW(path_output, str(ITERATION), initial_shape_HP, HP, active_cells, pob.x, n_var_1, n_var_2, n_var, 
-                          k_shape_1, k_shape_2, active_matriz, path_init_model, path_model, path_nwt_exe, path_obs_data)
-pob.x_best = np.copy(pob.x)
-pob.y = y_init
-pob.y_best = y_init
+    with h5py.File('Pre_ADPSO-CL.h5', 'r') as f:
+        pob.x = np.copy(f["pob_x"][VM-2])
+    f.close()
 
-#---    Guarda file con resultados
-df_results = pd.DataFrame(columns = ('pob_x', 'pob_y', 'pob_x_best', 'pob_y_best'))
+    y_init = Run_WEAP_MODFLOW(path_output, str(ITERATION), initial_shape_HP, HP, active_cells, pob.x, n_var_1, n_var_2, n_var, 
+                            k_shape_1, k_shape_2, active_matriz, path_init_model, path_model, path_nwt_exe, path_obs_data)
+    pob.x_best = np.copy(pob.x)
+    pob.y = y_init
+    pob.y_best = y_init
 
-#---    Guarda iter_0 results
-df_results.loc[int(ITERATION), 'pob_x'] = np.copy(pob.x)
-df_results.loc[int(ITERATION), 'pob_y'] = pob.y
-df_results.loc[int(ITERATION), 'pob_x_best'] = np.copy(pob.x_best)
-df_results.loc[int(ITERATION), 'pob_y_best'] = pob.y_best
-df_results.to_csv(path_output + '/ADPSO_CL_register_vm' + str(VM) + '.csv')
-
-#---    Siguientes iteraciones
-ITERATION += 1
-
-#---    PSO setup
-α = 0.8                                                     # Cognitive scaling parameter  # 0.8 # 1.49
-β = 0.8                                                     # Social scaling parameter     # 0.8 # 1.49                       
-w_min = 0.4                                                 # minimum value for the inertia velocity
-w_max = 0.9                                                 # maximum value for the inertia velocity
-vMax = np.around(np.multiply(u_bounds-l_bounds,0.4),4)      # Max velocity # De 0.8 a 0.4 # E2 con 0.4
-vMin = -vMax
-w = 0.5                                                     # inertia velocity
-
-for it in range(ITERATION, FINAL_ITERATION):
-    
-    time.sleep(np.random.randint(10,20,size = 1)[0])
-    gbest = send_request_py(IP_SERVER_ADD, pob.y, pob.x)           # Update global particle
-
-    #---    Update particle velocity
-    ϵ1,ϵ2 = np.around(np.random.uniform(),4), np.around(np.random.uniform(),4)            # [0, 1]
-
-    pob.v = np.around(np.around(w*pob.v,4) + np.around(α*ϵ1*(pob.x_best - pob.x),4) + np.around(β*ϵ2*(gbest - pob.x),4),4)
-
-    #---    Adjust particle velocity
-    index_vMax = np.where(pob.v > vMax)
-    index_vMin = np.where(pob.v < vMin)
-
-    if np.array(index_vMax).size > 0:
-        pob.v[index_vMax] = vMax[index_vMax]
-    if np.array(index_vMin).size > 0:
-        pob.v[index_vMin] = vMin[index_vMin]
-
-    #---    Update particle position
-    pob.x += pob.v
-
-    #---    Adjust particle position
-    index_pMax = np.where(pob.x > u_bounds)
-    index_pMin = np.where(pob.x < l_bounds)
-
-    if np.array(index_pMax).size > 0:
-        pob.x[index_pMax] = u_bounds[index_pMax]
-    if np.array(index_pMin).size > 0:
-        pob.x[index_pMin] = l_bounds[index_pMin]
-
-    #---    Evaluate the fitnness function
-    y = Run_WEAP_MODFLOW(path_output, str(ITERATION), initial_shape_HP, HP, active_cells, pob.x, n_var_1, n_var_2, n_var, 
-                         k_shape_1, k_shape_2, active_matriz, path_init_model, path_model, path_nwt_exe, path_obs_data)
-    
-    if y < pob.y_best:
-        pob.x_best = np.copy(pob.x)
-        pob.y_best = y
-        pob.y = y
-    else:
-        pob.y = y
-
-    #---    Update the inertia velocity
-    w = w_max - (ITERATION) * ((w_max-w_min)/FINAL_ITERATION)
+    #---    Create iteration register file
+    with h5py.File('ADPSO_CL_register_vm' + str(VM) + '.h5', 'w') as f:
+        iter_h5py = f.create_dataset("iteration", (FINAL_ITERATION, 1))
+        pob_x_h5py = f.create_dataset("pob_x", (FINAL_ITERATION, n_var))
+        pob_y_h5py = f.create_dataset("pob_y", (FINAL_ITERATION, 1))
+        pob_v_h5py = f.create_dataset("pob_v", (FINAL_ITERATION, n_var))
+        pob_x_best_h5py = f.create_dataset("pob_x_best", (FINAL_ITERATION, n_var))
+        pob_y_best_h5py = f.create_dataset("pob_y_best", (FINAL_ITERATION, 1))
+        pob_w_h5py = f.create_dataset("w", (FINAL_ITERATION, 1))
 
     #---    Iteration register
-    df_results.loc[int(ITERATION), 'pob_x'] = np.copy(pob.x)
-    df_results.loc[int(ITERATION), 'pob_y'] = pob.y
-    df_results.loc[int(ITERATION), 'pob_x_best'] = np.copy(pob.x_best)
-    df_results.loc[int(ITERATION), 'pob_y_best'] = pob.y_best
-    df_results.to_csv(path_output + '/ADPSO_CL_register_vm' + str(VM) + '.csv')
+        iter_h5py[0] = ITERATION
+        pob_x_h5py[0] = np.copy(pob.x)
+        pob_y_h5py[0] = pob.y
+        pob_v_h5py[0] = np.copy(pob.v)
+        pob_x_best_h5py[0] = np.copy(pob.x_best)
+        pob_y_best_h5py[0] = pob.y_best
+        pob_w_h5py[0] = 0.5
+    f.close()
 
     ITERATION += 1
 
-df_results.to_csv(path_output + '/ADPSO_CL_register_vm' + str(VM) + '.csv')
+        #---    PSO setup
+    α = 0.8                                                     # Cognitive scaling parameter  # 0.8 # 1.49
+    β = 0.8                                                     # Social scaling parameter     # 0.8 # 1.49                       
+    w_min = 0.4                                                 # minimum value for the inertia velocity
+    w_max = 0.9                                                 # maximum value for the inertia velocity
+    vMax = np.around(np.multiply(u_bounds-l_bounds,0.4),4)      # Max velocity # De 0.8 a 0.4 # E2 con 0.4
+    vMin = -vMax
+    w = 0.5                                                     # inertia velocity
+
+    for it in range(ITERATION, FINAL_ITERATION):
+        
+        time.sleep(np.random.randint(10,20,size = 1)[0])
+        gbest = send_request_py(IP_SERVER_ADD, pob.y, pob.x)           # Update global particle
+
+        #---    Update particle velocity
+        ϵ1,ϵ2 = np.around(np.random.uniform(),4), np.around(np.random.uniform(),4)            # [0, 1]
+
+        pob.v = np.around(np.around(w*pob.v,4) + np.around(α*ϵ1*(pob.x_best - pob.x),4) + np.around(β*ϵ2*(gbest - pob.x),4),4)
+
+        #---    Adjust particle velocity
+        index_vMax = np.where(pob.v > vMax)
+        index_vMin = np.where(pob.v < vMin)
+
+        if np.array(index_vMax).size > 0:
+            pob.v[index_vMax] = vMax[index_vMax]
+        if np.array(index_vMin).size > 0:
+            pob.v[index_vMin] = vMin[index_vMin]
+
+        #---    Update particle position
+        pob.x += pob.v
+
+        #---    Adjust particle position
+        index_pMax = np.where(pob.x > u_bounds)
+        index_pMin = np.where(pob.x < l_bounds)
+
+        if np.array(index_pMax).size > 0:
+            pob.x[index_pMax] = u_bounds[index_pMax]
+        if np.array(index_pMin).size > 0:
+            pob.x[index_pMin] = l_bounds[index_pMin]
+
+        #---    Evaluate the fitnness function
+        y = Run_WEAP_MODFLOW(path_output, str(ITERATION), initial_shape_HP, HP, active_cells, pob.x, n_var_1, n_var_2, n_var, 
+                            k_shape_1, k_shape_2, active_matriz, path_init_model, path_model, path_nwt_exe, path_obs_data)
+        
+        if y < pob.y_best:
+            pob.x_best = np.copy(pob.x)
+            pob.y_best = y
+            pob.y = y
+        else:
+            pob.y = y
+
+        #---    Update the inertia velocity
+        w = w_max - (ITERATION) * ((w_max-w_min)/FINAL_ITERATION)
+
+        #---    Iteration register
+        with h5py.File('ADPSO_CL_register_vm' + str(VM) + '.h5', 'a') as f:
+            f["iteration"][ITERATION] = ITERATION
+            f["pob_x"][ITERATION] = np.copy(pob.x)
+            f["pob_y"][ITERATION] = pob.y
+            f["pob_v"][ITERATION] = np.copy(pob.v)
+            f["pob_x_best"][ITERATION] = np.copy(pob.x_best)
+            f["pob_y_best"][ITERATION] = pob.y_best
+
+            f["w"][ITERATION] = w
+        f.close()
+
+        ITERATION += 1
+
+#---    Siguientes iteraciones
+else:
+    #---    PSO setup
+    α = 0.8                                                     # Cognitive scaling parameter  # 0.8 # 1.49
+    β = 0.8                                                     # Social scaling parameter     # 0.8 # 1.49                       
+    w_min = 0.4                                                 # minimum value for the inertia velocity
+    w_max = 0.9                                                 # maximum value for the inertia velocity
+    vMax = np.around(np.multiply(u_bounds-l_bounds,0.4),4)      # Max velocity # De 0.8 a 0.4 # E2 con 0.4
+    vMin = -vMax
+    w = 0.5                                                     # inertia velocity
+
+    with h5py.File('ADPSO_CL_register_vm' + str(VM) + '.h5', 'r') as f:
+        pob.x = np.copy(f["pob_x"][ITERATION - 1])
+        pob.y = f["pob_y"][ITERATION - 1]
+        pob.v = np.copy(f["pob_v"][ITERATION - 1])
+        pob.x_best = np.copy(f["pob_x_best"][ITERATION - 1])
+        pob.y_best = f["pob_y_best"][ITERATION - 1]
+
+        w = f["w"][ITERATION - 1]                               # inertia velocity
+    f.close()
+
+    for it in range(ITERATION, FINAL_ITERATION):
+        
+        time.sleep(np.random.randint(10,20,size = 1)[0])
+        gbest = send_request_py(IP_SERVER_ADD, pob.y, pob.x)           # Update global particle
+
+        #---    Update particle velocity
+        ϵ1,ϵ2 = np.around(np.random.uniform(),4), np.around(np.random.uniform(),4)            # [0, 1]
+
+        pob.v = np.around(np.around(w*pob.v,4) + np.around(α*ϵ1*(pob.x_best - pob.x),4) + np.around(β*ϵ2*(gbest - pob.x),4),4)
+
+        #---    Adjust particle velocity
+        index_vMax = np.where(pob.v > vMax)
+        index_vMin = np.where(pob.v < vMin)
+
+        if np.array(index_vMax).size > 0:
+            pob.v[index_vMax] = vMax[index_vMax]
+        if np.array(index_vMin).size > 0:
+            pob.v[index_vMin] = vMin[index_vMin]
+
+        #---    Update particle position
+        pob.x += pob.v
+
+        #---    Adjust particle position
+        index_pMax = np.where(pob.x > u_bounds)
+        index_pMin = np.where(pob.x < l_bounds)
+
+        if np.array(index_pMax).size > 0:
+            pob.x[index_pMax] = u_bounds[index_pMax]
+        if np.array(index_pMin).size > 0:
+            pob.x[index_pMin] = l_bounds[index_pMin]
+
+        #---    Evaluate the fitnness function
+        y = Run_WEAP_MODFLOW(path_output, str(ITERATION), initial_shape_HP, HP, active_cells, pob.x, n_var_1, n_var_2, n_var, 
+                            k_shape_1, k_shape_2, active_matriz, path_init_model, path_model, path_nwt_exe, path_obs_data)
+        
+        if y < pob.y_best:
+            pob.x_best = np.copy(pob.x)
+            pob.y_best = y
+            pob.y = y
+        else:
+            pob.y = y
+
+        #---    Update the inertia velocity
+        w = w_max - (ITERATION) * ((w_max-w_min)/FINAL_ITERATION)
+
+        #---    Iteration register
+        with h5py.File('ADPSO_CL_register_vm' + str(VM) + '.h5', 'a') as f:
+            f["iteration"][ITERATION] = ITERATION
+            f["pob_x"][ITERATION] = np.copy(pob.x)
+            f["pob_y"][ITERATION] = pob.y
+            f["pob_v"][ITERATION] = np.copy(pob.v)
+            f["pob_x_best"][ITERATION] = np.copy(pob.x_best)
+            f["pob_y_best"][ITERATION] = pob.y_best
+
+            f["w"][ITERATION] = w
+        f.close()
+
+        ITERATION += 1
