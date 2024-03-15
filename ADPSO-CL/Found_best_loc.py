b0d183b8ba2b02b73c06ceb14aa8e9bc1ae4aef0
KiaraTesen <katesen@uc.cl>
2024-03-15 09:27:16 -0500

    Edit FBL

--- ADPSO-CL/Found_best_loc.py
+++ ADPSO-CL/Found_best_loc.py
@@ -4,9 +4,9 @@ import sys
 import pandas as pd
 import numpy as np
 
-#path_output = r'C:\Users\vagrant\Documents\WEAPMODFLOW_LP_Calibration\ADPSO-CL\output'
-path_output = r'C:\Users\aimee\Desktop\Github\WEAPMODFLOW_LP_Calibration\ADPSO-CL\output'
-total_vms = 1
+path_output = r'C:\Users\vagrant\Documents\WEAPMODFLOW_LP_Calibration\ADPSO-CL\output'
+#path_output = r'C:\Users\aimee\Desktop\Github\WEAPMODFLOW_LP_Calibration\ADPSO-CL\output'
+total_vms = 15
 
 df_y = pd.DataFrame()
 iter_vm_pre = 0
@@ -26,9 +26,9 @@ for i in range (2, int(total_vms + 2)):
         #g_best_selected = f["pob_x"][3]
     f.close()
 
-    indices = np.where(y != 0)
-    print(len(indices[1]))
-    print(y[len(indices[1])-1])
+    #indices = np.where(y != 0)
+    #print(len(indices[1]))
+    #print(y[len(indices[1])-1])
  
     
     #print(g_best_selected)
