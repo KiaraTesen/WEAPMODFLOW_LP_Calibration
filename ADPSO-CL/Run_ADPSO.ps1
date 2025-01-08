bca7a4168308579d17a7d00a16f0523484197d28
KiaraTesen <katesen@uc.cl>
2025-01-08 16:22:37 -0500

    Correct the IP server

--- ADPSO-CL/Run_ADPSO.ps1
+++ ADPSO-CL/Run_ADPSO.ps1
@@ -4,4 +4,4 @@ $total_iteration = [int]($p1)
 $final_iteration = [int]($p2)
 $vm = [int]((hostname) -replace '\D+(\d+)','$1')
 
-python Methodology_ADPSO_CL.py 10.0.0.11:8888 $total_iteration $final_iteration $vm
+python Methodology_ADPSO_CL.py 10.0.1.11:8888 $total_iteration $final_iteration $vm
